61fc096bb7b48ebd6dad9ea4fbf1b772e21ed743
Milki12 <74327478+Milki12@users.noreply.github.com>
2020-11-16 16:30:53 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,32 @@
-Hey how thanks for instaljng this tool.
+
+#!/bin/bash
+
+printf '\033]2;install.sh\a'
+
+G="\033[1;34m[*] \033[0m"
+S="\033[1;32m[+] \033[0m"
+E="\033[1;31m[-] \033[0m"
+
+if [[ $(id -u) != 0 ]]; then
+    echo -e ""$E"Access Granted!"
+    exit
+fi
+
+{
+    CHECK="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+} &> /dev/null
+
+if [[ "$CHECK" != 0 ]]; then 
+    echo -e ""$E"No Internet connection!"
+    exit
+fi
+
+sleep 0.5
+clear
+sleep 0.5
+cat banner/banner.txt
+echo
+
+sleep 1
+echo -e ""$G"Installing dependencies..."
+sleep 1
